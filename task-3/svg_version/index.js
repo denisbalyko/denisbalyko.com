cc5e57cafe180b9d6c140ce061f854e194ecfe87
denisbalyko <denisbalyko@gmail.com>
2016-03-12 19:15:44 +0300

    remove css_version and js_version tailcode, bye bye canvas

--- task-3/svg_version/index.js
+++ task-3/svg_version/index.js
@@ -1,62 +1,45 @@
 (function () {
 
     var video,
-        canvas,
         controlsFilter,
         filterName;
 
     var initVideoStream = function(){
-        video = document.querySelector('.camera__video'),
-        canvas = document.querySelector('.camera__canvas'),
-        controlsFilter = document.querySelector('.controls__filter'),
-        filterName = controlsFilter.value,
-        prefixClass = 'camera__canvas_';
-
-        canvas.classList.add(prefixClass+filterName);
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
+        video = document.querySelector('.camera__video');
+        controlsFilter = document.querySelector('.controls__filter');
+        filterName = controlsFilter.value;
     }
 
     window.onload = initVideoStream();
     window.onresize = initVideoStream();
 
     controlsFilter.onchange = function(){
-        canvas.classList.remove(prefixClass+filterName);
         filterName = this.value;
-        canvas.classList.add(prefixClass+filterName);
+        video.style.webkitFilter='url(#'+filterName+')';
+        video.style.mozFilter='url(#'+filterName+')';
+        video.style.filter='url(#'+filterName+')';
     }
 
-    var getVideoStream = function (callback) {
-        navigator.getUserMedia = navigator.getUserMedia ||
-            navigator.webkitGetUserMedia ||
-            navigator.mozGetUserMedia;
+    controlsFilter.onchange();
 
-        if (navigator.getUserMedia) {
-            navigator.getUserMedia({video: true},
-                function (stream) {
-                    video.src = window.URL.createObjectURL(stream);
-                    video.onloadedmetadata = function (e) {
-                        video.play();
-                        canvas.width = video.videoWidth;
-                        canvas.height = video.videoHeight;
-                        callback();
-                    };
-                },
-                function (err) {
-                    console.log("The following error occured: " + err.name);
-                }
-            );
-        } else {
-            console.log("getUserMedia not supported");
-        }
-    };
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia;
 
-    var captureFrame = function () {
-        canvas.getContext('2d').drawImage(video, 0, 0);
-    };
+    if (navigator.getUserMedia) {
+        navigator.getUserMedia({video: true},
+            function (stream) {
+                video.src = window.URL.createObjectURL(stream);
+                video.onloadedmetadata = function (e) {
+                    video.play();
+                };
+            },
+            function (err) {
+                console.log("The following error occured: " + err.name);
+            }
+        );
+    } else {
+        console.log("getUserMedia not supported");
+    }
 
-    getVideoStream(function () {
-        captureFrame();
-        setInterval(captureFrame, 1);
-    });
 })();

9d3fa0be3b6617c7db2f4037620c547eacbc6203
denisbalyko <denisbalyko@gmail.com>
2016-06-11 13:48:26 +0300

    added uncss

--- gulpfile.js
+++ gulpfile.js
@@ -2,43 +2,47 @@ var gulp = require('gulp');
 var plumber = require('gulp-plumber');
 var htmlmin = require('gulp-htmlmin');
 var concat = require('gulp-concat');
-var cleanCSS = require('gulp-clean-css');
+var uncss = require('gulp-uncss');
+var nano = require('gulp-cssnano');
 var imageop = require('gulp-image-optimization');
 
-gulp.task('htmlminify', function() {
+gulp.task('htmlminify', function () {
     gulp.src('*.html')
         .pipe(plumber())
         .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('production'))
 });
 
-gulp.task('cssconcat', function() {
+gulp.task('cssconcat', function () {
     gulp.src(['css/reset.css',
-                     'css/normalize.css',
-                     'css/habit.css',
-                     'css/960.css',
-                     'css/768.css',
-                     'css/320.css'])
+              'css/normalize.css',
+              'css/habit.css',
+              'css/960.css',
+              'css/768.css',
+              'css/320.css'])
         .pipe(plumber())
         .pipe(concat('style.css'))
         .pipe(gulp.dest('.'))
 });
 
-gulp.task('cssminify', function() {
+gulp.task('cssminify', function () {
     gulp.src('style.css')
         .pipe(plumber())
-        .pipe(cleanCSS())
+        .pipe(uncss({
+            html: ['index.html']
+        }))
+        .pipe(nano())
         .pipe(gulp.dest('production'));
 });
 
-gulp.task('imgopt', function() {
+gulp.task('imgopt', function () {
     gulp.src(['favicon.png'])
-    .pipe(imageop({
-        optimizationLevel: 5,
-        progressive: true,
-        interlaced: true
-    }))
-    .pipe(gulp.dest('production'));
+        .pipe(imageop({
+            optimizationLevel: 5,
+            progressive: true,
+            interlaced: true
+        }))
+        .pipe(gulp.dest('production'));
 });
 
 gulp.task('default', ['cssconcat', 'cssminify', 'htmlminify', 'imgopt']);
